4981c7bb8af7ef08832df0b16e6f71f343122b8b
Ferdous <71073170+alexinferdous@users.noreply.github.com>
2023-10-13 22:28:02 -0700

    Complete Rock Paper Scissors game for one turn. User input needs to be improved. Loop needs to be implemented

--- javascript.js
+++ javascript.js
@@ -1,1 +1,54 @@
-console.log("hey");
+const userSelection2 = prompt("play: ");
+const userSelection = userSelection2.toUpperCase();
+
+
+if (userSelection !== "ROCK" && userSelection !== "PAPER" && userSelection !== "SCISSORS") {
+    location.reload();
+}
+
+
+function getComputerChoice() {
+    const strings = ["ROCK", "PAPER", "SCISSORS"];
+    const randomIndex = Math.floor(Math.random() * strings.length);
+    const randomString = strings[randomIndex];
+    return randomString;
+}
+
+
+function playRound(playerSelection) {
+   const computerSelection = getComputerChoice();
+    let result;
+   if (playerSelection === computerSelection) {
+        result = "It's a tie!";
+    }
+
+    if (playerSelection === "ROCK") {
+        if (computerSelection === "SCISSORS") {
+            result = "You win!";
+        } else if (computerSelection === "PAPER") {
+            result = "You lose!";
+        }
+    } else if(playerSelection === "PAPER") {
+        if (computerSelection === "ROCK") {
+            result = "You win!";
+        } else if (computerSelection === "SCISSORS") {
+            result = "You lose!";
+        }
+    } else {
+        if (computerSelection === "ROCK") {
+            result = "You lose!";
+        } else if (computerSelection === "PAPER") {
+            result = "You win!";
+        }
+    }
+
+    return [playerSelection, computerSelection, result];
+}
+
+
+const [op1, op2, op3] = playRound(userSelection);
+
+
+console.log("You chose: ", op1);
+console.log("Computer chose: ", op2);
+console.log("Result: ", op3);
