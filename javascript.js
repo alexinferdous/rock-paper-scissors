fa36bf4030177a367e9c43899df44c514a413f1a
Ferdous <71073170+alexinferdous@users.noreply.github.com>
2023-10-31 21:15:43 -0700

    Complete rock paper scissors with UI. Lots of room for improvement though

--- javascript.js
+++ javascript.js
@@ -1,69 +1,145 @@
-function getUserInput() {
-    const userSelection2 = prompt("play: ");
-    const userSelection3 = userSelection2.toUpperCase();
-    let userSelection4;
-
-    if (userSelection3 === "ROCK" || userSelection3 === "PAPER" || userSelection3 === "SCISSORS") {
-        userSelection4 = userSelection3;
-    } else if (userSelection3 !== "ROCK" && userSelection3 !== "PAPER" && userSelection3 !== "SCISSORS") {
-        alert("Please choose anyone from rock/paper/scissors")
-        const userSelection2 = prompt("play: ");
-        userSelection4 = userSelection2.toUpperCase();
-        
-    }
-    return userSelection4;
-
-}
-
 function getComputerChoice() {
-    const strings = ["ROCK", "PAPER", "SCISSORS"];
+    const strings = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * strings.length);
     const randomString = strings[randomIndex];
     return randomString;
 }
 
-
-function playRound(playerSelection) {
-   const computerSelection = getComputerChoice();
+function forRock() {
+    const computerChoice = getComputerChoice();
     let result;
-   if (playerSelection === computerSelection) {
-        result = "It's a tie!";
-    }
-
-    if (playerSelection === "ROCK") {
-        if (computerSelection === "SCISSORS") {
-            result = "You win!";
-        } else if (computerSelection === "PAPER") {
-            result = "You lose!";
-        }
-    } else if(playerSelection === "PAPER") {
-        if (computerSelection === "ROCK") {
-            result = "You win!";
-        } else if (computerSelection === "SCISSORS") {
-            result = "You lose!";
-        }
+    if (computerChoice === "rock"){
+        result = "It's a Tie!";
+    } else if (computerChoice === "paper") {
+        result = "You Lose!";
     } else {
-        if (computerSelection === "ROCK") {
-            result = "You lose!";
-        } else if (computerSelection === "PAPER") {
-            result = "You win!";
-        }
+        result = "You Win!";
     }
-
-    return [playerSelection, computerSelection, result];
+    return result;
 }
 
-
-
-function fiveRounds() {
-    for (let i=0; i<5; i++) {
-        const userSelection = getUserInput();
-        const [op1, op2, op3] = playRound(userSelection);
-        console.log("You chose: ", op1);
-        console.log("Computer chose: ", op2);
-        console.log("Result: ", op3);
+function forPaper() {
+    const computerChoice = getComputerChoice();
+    let result;
+    if (computerChoice === "paper"){
+        result = "It's a Tie!";
+    } else if (computerChoice === "scissors") {
+        result = "You Lose!";
+    } else {
+        result = "You Win!";
     }
+    return result;
 }
 
+function forScissors() {
+    const computerChoice = getComputerChoice();
+    let result;
+    if (computerChoice === "scissors"){
+        result = "It's a Tie!";
+    } else if (computerChoice === "rock") {
+        result = "You Lose!";
+    } else {
+        result = "You Win!";
+    }
+    return result;
+}
 
-fiveRounds();
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+
+let loseCount = 0;
+let winCount = 0;
+let winQuote = "";
+
+function rockClick() {
+    if (rockButton.style.backgroundColor === "blue"){
+        rockButton.style.backgroundColor = "white";
+    } else {
+    rockButton.style.backgroundColor = "blue";
+    }
+    const result = forRock();
+    if (result === "You Win!") {
+        winCount = winCount + 1;
+    } else if (result === "You Lose!") {
+        loseCount = loseCount + 1;
+    }
+
+    if(winCount === 5){
+        winQuote = "You are the winner! Congrats!"
+    } else if (loseCount === 5) {
+        winQuote = "Computer is the winner. Better luck next time";
+    } else {
+        winQuote = "";
+    }
+
+
+    const selector = document.getElementById("newDiv");
+    const showResult = document.createElement("p");
+    showResult.textContent = result + "   Number of Wins: " + winCount + "   Number of Losses: " + loseCount + "   " + winQuote;
+    selector.appendChild(showResult);
+
+}
+
+function paperClick() {
+    if (paperButton.style.backgroundColor === "blue"){
+        paperButton.style.backgroundColor = "white";
+    } else {
+    paperButton.style.backgroundColor = "blue";
+    }
+    const result = forPaper();
+    if (result === "You Win!") {
+        winCount = winCount + 1;
+    } else if (result === "You Lose!") {
+        loseCount = loseCount + 1;
+    }
+
+    if(winCount === 5){
+        winQuote = "You are the winner! Congrats!"
+    } else if (loseCount === 5) {
+        winQuote = "Computer is the winner. Better luck next time";
+    } else {
+        winQuote = "";
+    }
+
+
+    const selector = document.getElementById("newDiv");
+    const showResult = document.createElement("p");
+    showResult.textContent = result + "   Number of Wins: " + winCount + "   Number of Losses: " + loseCount + "   " + winQuote;
+    selector.appendChild(showResult);
+
+}
+
+function scissorsClick() {
+    if (scissorsButton.style.backgroundColor === "blue"){
+        scissorsButton.style.backgroundColor = "white";
+    } else {
+        scissorsButton.style.backgroundColor = "blue";
+    }
+    const result = forScissors();
+    if (result === "You Win!") {
+        winCount = winCount + 1;
+    } else if (result === "You Lose!") {
+        loseCount = loseCount + 1;
+    }
+
+    if(winCount === 5){
+        winQuote = "You are the winner! Congrats!"
+    } else if (loseCount === 5) {
+        winQuote = "Computer is the winner. Better luck next time";
+    } else {
+        winQuote = "";
+    }
+
+
+    const selector = document.getElementById("newDiv");
+    const showResult = document.createElement("p");
+    showResult.textContent = result + "   Number of Wins: " + winCount + "   Number of Losses: " + loseCount + "   " + winQuote;
+    selector.appendChild(showResult);
+
+}
+
+rockButton.addEventListener('click', rockClick);
+paperButton.addEventListener('click', paperClick);
+scissorsButton.addEventListener('click', scissorsClick);
+
